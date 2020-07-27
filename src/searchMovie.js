9626d05491024ec3fcab3a814d75a094a939dd5e
bipon68 <bipon68@users.noreply.github.com>
2020-07-27 18:43:26 +0600

    try catch added

--- src/searchMovie.js
+++ src/searchMovie.js
@@ -10,9 +10,15 @@ const SearchMovies = () => {
         const query = "Jurassic Park";
         const url = `https://api.themoviedb.org/3/search/movie?api_key=4679b9815a4209c0c0b34d4ef5071d0f&language=en-US&query=${query}&page=1&include_adult=false`;
         
-        const res = await fetch(url);
-        const data = await res.json();
-        console.log(data.results);
+        try{
+            const res = await fetch(url);
+            const data = await res.json();
+            console.log(data.results);
+        }catch(err){
+            console.error(err)
+        }
+        
+        
     
     }
 
